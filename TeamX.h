0c707ce1dbca5c14b2ccfc1f04413618cd83221e
= <=>
2015-05-29 23:44:37 -0400

    Compilation achieved

--- TeamX.h
+++ TeamX.h
@@ -1,7 +1,9 @@
 #include <chrono>
 
 #include <climits>
+#include <vector>
 #include "ttt3d.h"
+using namespace std;
 /*
     Due June 1, 2015
     Each player gets a total of 3 minutes as measured by time_used()
@@ -9,7 +11,8 @@
 */
 
 class TeamX : public TTT3D {
-public:
+public:  
+
     explicit TeamX(const duration<double> tta) : TTT3D(tta) {
 
     }
@@ -18,18 +21,37 @@ public:
 
     }
 
-    struct node{
-        int b[4][4][4];
-    };
+private:
+    struct Node{
 
-    int alphabeta(node n, int depth, int alpha, int beta, bool maximizingPlayer) {
-        if (depth == 0 || node.isTerminal()) {
-            return node.heuristic();
+        typedef vector<int> vi;
+        typedef vector<vi> vii;
+        typedef vector<vii> grid;
+
+        grid b; //1 is us, -1 is opponent, 0 is unoccupied
+        Node(){
+            b = grid(4, vii(4, vi(4, 0)));
         }
+        bool isTerminal(){
+            return true;
+        }
+        int heuristic(){
+            return 0;
+        }
+        vector<Node> children(){
+
+        }
+    };
+    typedef vector<Node> vN;
+    int alphabeta(Node n, int depth, int alpha, int beta, bool maximizingPlayer) {
+        if (depth == 0 || n.isTerminal()) {
+            return n.heuristic();
+        }
+        vN children = n.children();
         if (maximizingPlayer) {
             int v = INT_MIN;
-            for (int i = 0; i < n.childs; ++i) {
-                v = max(v, alphabeta(n.child[i], depth - 1, alpha, beta, false));
+            for (int i = 0; i < children.size(); ++i) {
+                v = max(v, alphabeta(children[i], depth - 1, alpha, beta, false));
                 int a = max(alpha, v);
                 if (beta <= alpha) {
                     break;
@@ -39,8 +61,8 @@ public:
         }
         else {
             int v = INT_MAX;
-            for (int i = 0; i < n.childs. ++i) {
-                v = min(v, alphabeta(child, depth - 1, alpha, beta, true));
+            for (int i = 0; i < children.size(); ++i) {
+                v = min(v, alphabeta(children[i], depth - 1, alpha, beta, true));
                 beta = min(beta, v);
                 if (beta <= alpha) {
                     break;
@@ -49,9 +71,5 @@ public:
             return v;
         }
     }
-
-private:
-
-
 
 };
