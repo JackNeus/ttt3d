e31c056e4677591eddf6407870738def3e79dddf
= <=>
2015-05-29 23:26:41 -0400

    Only compile errors now are from incomplete node class

--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
-#include "ttt3d.h"
 #include "TeamX.h"
-
-class TeamX : public TTT3D {
-public:
-    explicit TeamX(const duration<double> tta) : TTT3D(tta) {
-
-    }
-
-    void next_move(int mv[]) {
-
-    }
-};
 
 int main() {
     auto length = minutes(3);
